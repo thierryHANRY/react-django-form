0b7d7823c8c3d4183ce78e9de3431d11aea3d8a6
WOGUE FEUTSI THIERRY <woguefeutsithierry@gmail.com>
2026-01-05 12:29:17 +0100

    Add React frontend and connect with Django backend

--- backend/auth/settings.py
+++ backend/auth/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'users'
 ]
 
 MIDDLEWARE = [
+     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +52,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+]
+
 
 ROOT_URLCONF = 'auth.urls'
 
