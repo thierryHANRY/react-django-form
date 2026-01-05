65582ff2090be0fb4ca7ff7f08dd12e21f3e583c
WOGUE FEUTSI THIERRY <woguefeutsithierry@gmail.com>
2026-01-05 19:43:25 +0100

    create page navigation with react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Navbar from './components/Navbar.jsx'
+import {Routes, Route, useLocation} from 'react-router-dom'
+import Home from './components/Home.jsx'
+import Login from './components/Login.jsx'
+import Register from './components/Register.jsx'
+import About from './components/About.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const location = useLocation();
+  const noNavbar = location.pathname === '/register' || location.pathname === '/'
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      {
+        noNavbar ? 
+          <Routes>
+              <Route path='/' element={<Login/>}/>
+              <Route path='/register' element={<Register/>}/>
+         </Routes>
+        : 
+
+        <Navbar
+        content={
+          <Routes>
+            <Route>
+              <Route path='/home' element={<Home/>}/>
+              <Route path='/about' element={<About/>}/>
+            </Route>
+         </Routes>
+        }
+      />
+      }
     </>
   )
 }
